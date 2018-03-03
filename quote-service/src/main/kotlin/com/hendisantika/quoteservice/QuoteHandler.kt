d332afb5f82860613c8bda2ff96c531c42b1440d
Hendi Santika <hendisantika@gmail.com>
2018-03-03 18:28:05 +0700

    #5 - Add Quote Handler

---
+++ quote-service/src/main/kotlin/com/hendisantika/quoteservice/QuoteHandler.kt
@@ -0,0 +1,29 @@
+package com.hendisantika.quoteservice
+
+import org.omg.CORBA.ServerRequest
+import org.springframework.stereotype.Component
+import java.time.Duration.ofMillis
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : demo-webflux-streaming
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/03/18
+ * Time: 18.27
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Component
+class QuoteHandler(val quoteGenerator: QuoteGenerator) {
+
+    fun fetchQuotesSSE(req: ServerRequest) = ok()
+            .contentType(TEXT_EVENT_STREAM)
+            .body(quoteGenerator.fetchQuoteStream(ofMillis(200)), Quote::class.java)
+
+    fun fetchQuotes(req: ServerRequest) = ok()
+            .contentType(APPLICATION_STREAM_JSON)
+            .body(quoteGenerator.fetchQuoteStream(ofMillis(200)), Quote::class.java)
+
+}
