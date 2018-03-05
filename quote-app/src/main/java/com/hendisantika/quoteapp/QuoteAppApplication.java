5de6c164a9b58ec2242adc0102198557a51f160f
Hendi Santika <hendisantika@gmail.com>
2018-03-05 20:55:00 +0700

    #10 - Add quote-app

---
+++ quote-app/src/main/java/com/hendisantika/quoteapp/QuoteAppApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.quoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteAppApplication.class, args);
+    }
+}
